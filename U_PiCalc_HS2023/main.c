03e0ea4030fdeccd4a1612645bc71411816dc2b4
Twister10000 <82016318+Twister10000@users.noreply.github.com>
2023-10-04 17:54:46 +0200

    Set up Button reading

--- U_PiCalc_HS2023/main.c
+++ U_PiCalc_HS2023/main.c
@@ -30,8 +30,31 @@
 
 #include "ButtonHandler.h"
 
+<<<<<<< Updated upstream
+=======
+#define EVBUTTONS_S1	1<<0
+#define EVBUTTONS_S2	1<<1
+#define EVBUTTONS_S3	1<<2
+#define EVBUTTONS_S4	1<<3
+#define EVBUTTONS_L1	1<<4
+#define EVBUTTONS_L2	1<<5
+#define EVBUTTONS_L3	1<<6
+#define EVBUTTONS_L4	1<<7
+#define EVBUTTONS_CLEAR	0xFF
+evButtonState = xEventGroupCreate();
 
 void controllerTask(void* pvParameters);
+void vUserInterface(void *pvParameters);
+void vPICalcLeibniz(void *pvParameters);
+void vPICalcNila(void *pvParameters);
+
+TaskHandle_t UserInt;
+TaskHandle_t LeibnizCalc;
+TaskHandle_t NilaCalc;
+EventGroupHandle_t evButtonState;
+>>>>>>> Stashed changes
+
+uint32_t buttonstate = 0;
 
 int main(void)
 {
@@ -52,30 +75,31 @@ void controllerTask(void* pvParameters) {
 	for(;;) {
 		updateButtons();
 		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
-			char pistring[12];
+			/*char pistring[12];
 			sprintf(&pistring[0], "PI: %.8f", M_PI);
-			vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			vDisplayWriteStringAtPos(1,0, "%s", pistring);*/
+			xEventGroupSetBits(evButtonState, EVBUTTONS_S1);
 		}
 		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_S2);
 		}
 		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_S3);
 		}
 		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_S4);
 		}
 		if(getButtonPress(BUTTON1) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_L1);
 		}
 		if(getButtonPress(BUTTON2) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_L2);
 		}
 		if(getButtonPress(BUTTON3) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_L3);
 		}
 		if(getButtonPress(BUTTON4) == LONG_PRESSED) {
-			
+			xEventGroupSetBits(evButtonState, EVBUTTONS_L4);
 		}
 		vTaskDelay(10/portTICK_RATE_MS);
 	}
